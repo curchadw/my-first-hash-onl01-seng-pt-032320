b468b04f3a1799832294c4f5296bc1f453806d51
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 20:30:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash(key,value)
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = key => value 
-  return your_hash
+ your_hash
   
 end
 
